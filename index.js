85a335cce4ced18c17fa49247cb2f05f64ae4ea1
Matty Williams <mattyqwilliams@gmail.com>
2023-05-03 16:56:08 -0700

    Update index.js
    Update for the markdown version of Fetch

--- index.js
+++ index.js
@@ -2,148 +2,89 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const { JSDOM } = require('jsdom');
+const TurndownService = require('turndown');
 const app = express();
-const vm = require('vm'); // Import the 'vm' module for creating a sandbox
 
-const rateLimitMap = new Map(); // In-memory rate limit map
-const RATE_LIMIT = 10; // Max requests per minute
-const RATE_WINDOW = 60 * 1000; // 1 minute in milliseconds
+const rateLimitMap = new Map();
+const RATE_LIMIT = 10;
+const RATE_WINDOW = 60 * 1000;
+const MAX_CONTENT_LENGTH = 90000;
+const EDGE_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36';
+const UNNECESSARY_ELEMENTS = ['head', 'script', 'style'];
 
 app.use(express.json());
 app.use(express.static('public'));
 
 const edgeHeaders = {
-  'User-Agent':
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3 Edge/16.16299',
+  'User-Agent': EDGE_USER_AGENT,
   'Accept-Language': 'en-US,en;q=0.9',
   'Accept-Encoding': 'gzip, deflate, br',
   'Connection': 'keep-alive',
 };
 
-// Middleware to enforce rate limiting
 app.use((req, res, next) => {
-  const clientIP = req.ip; // Use client IP as identifier
+  const clientIP = req.ip;
   const currentTime = Date.now();
-
-  const entry = rateLimitMap.get(clientIP);
-  if (entry) {
-    const { count, timestamp } = entry;
-    // Check if the rate window has expired
-    if (currentTime - timestamp > RATE_WINDOW) {
-      // Reset the count and timestamp for the new rate window
-      rateLimitMap.set(clientIP, { count: 1, timestamp: currentTime });
-      return next();
-    }
-    // Check if the client has exceeded the allowed rate limit
-    if (count >= RATE_LIMIT) {
-      return res.status(429).send('Too many requests. Please try again later.');
-    }
-    // Update request count in the map
-    rateLimitMap.set(clientIP, { count: count + 1, timestamp });
-  } else {
-    // Create a new entry for the client IP
-    rateLimitMap.set(clientIP, { count: 1, timestamp: currentTime });
+  const entry = rateLimitMap.get(clientIP) || { count: 0, timestamp: currentTime };
+  if (currentTime - entry.timestamp > RATE_WINDOW || entry.count < RATE_LIMIT) {
+    rateLimitMap.set(clientIP, { count: entry.count + 1, timestamp: currentTime });
+    return next();
   }
-
-  next();
+  return res.status(429).send('Too many requests. Please try again later.');
 });
 
-// Apply CORS middleware globally to all routes
 app.use(cors());
 
-const PAGE_SIZE = 60000; // Reduced page size
+const maxContentLength = MAX_CONTENT_LENGTH - 100;
+const PAGE_SIZE = maxContentLength;
 
-// GET endpoint to fetch the content of a website
+function sanitizeContent(content) {
+  const dom = new JSDOM(content);
+  const { window } = dom;
+  const { document } = window;
+  UNNECESSARY_ELEMENTS.forEach((unnecessaryElement) => {
+    const elements = document.querySelectorAll(unnecessaryElement);
+    elements.forEach((element) => {
+      element.remove();
+    });
+  });
+  const elements = document.querySelectorAll('*');
+  elements.forEach((element) => {
+    for (const attr of element.attributes) {
+      if (!['class', 'id', 'href'].includes(attr.name)) {
+        element.removeAttribute(attr.name);
+      }
+    }
+  });
+  return dom.serialize();
+}
+
+const turndownService = new TurndownService();
+
 app.get('/fetchContent', async (request, response) => {
   const targetUrl = request.query.url;
-  const page = parseInt(request.query.page) || 1;
-  const section = request.query.section || 'body'; // Default to 'body' section
+  const chunk = parseInt(request.query.chunk) || 1; // Renamed "page" to "chunk"
 
   try {
     const webContentResponse = await axios.get(targetUrl, { headers: edgeHeaders });
     const dom = new JSDOM(webContentResponse.data);
-    const headContent = dom.window.document.head.outerHTML;
     const bodyContent = dom.window.document.body.outerHTML;
+    const sanitizedContent = sanitizeContent(bodyContent);
+    const markdownContent = turndownService.turndown(sanitizedContent);
+    const totalPages = Math.ceil(markdownContent.length / PAGE_SIZE);
+    const start = (chunk - 1) * PAGE_SIZE; // Updated variable name
+    const end = chunk * PAGE_SIZE; // Updated variable name
+    const contentPage = markdownContent.slice(start, end);
+    const nextPage = chunk < totalPages ? `${request.protocol}://${request.get('host')}${request.path}?url=${encodeURIComponent(targetUrl)}&chunk=${chunk + 1}` : null; // Updated parameter name
+    const morePages = totalPages - chunk; // Updated variable name
+    const nextPageMessage = morePages > 0 ? `This response has been chunked due to its length. There are ${morePages} more pages. To access the next chunk, fetch this URL: ${nextPage}\n\n` : '';
 
-    // Choose the content section based on the query parameter
-    const selectedContent = section === 'head' ? headContent : bodyContent;
+    const result = nextPageMessage + "\n\n" + contentPage;
 
-    // Paginate the selected content
-    const totalPages = Math.ceil(selectedContent.length / PAGE_SIZE);
-    const start = (page - 1) * PAGE_SIZE;
-    const end = page * PAGE_SIZE;
-    const contentPage = selectedContent.slice(start, end);
-
-    // Generate URLs for each page
-    const pageUrls = Array.from({ length: totalPages }, (_, i) => {
-      return `${request.protocol}://${request.get('host')}${request.path}?url=${encodeURIComponent(targetUrl)}&section=${section}&page=${i + 1}`;
-    });
-
-    const result = {
-      content: contentPage,
-      page: page,
-      totalPages: totalPages,
-      pageUrls: pageUrls
-    };
-
-    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('Content-Type', 'text/plain');
     response.send(result);
   } catch (error) {
     response.status(500).send('Error fetching website content.');
-  }
-});
-
-// GET endpoint to execute a custom JavaScript function on a website's content
-app.get('/executeFunction', async (request, response) => {
-  const targetUrl = request.query.url;
-  const customFunctionBody = request.query.functionBody;
-  const page = parseInt(request.query.page) || 1;
-
-  if (!customFunctionBody) {
-    return response.status(400).send('Function body is missing in the query parameters.');
-  }
-
-  try {
-    const webContentResponse = await axios.get(targetUrl, { headers: edgeHeaders });
-    const dom = new JSDOM(webContentResponse.data);
-    const { window } = dom;
-    const { document } = window;
-    // Create a sandbox context for executing the custom function
-    const sandbox = {
-      document,
-      result: undefined
-    };
-    // Wrap the customFunctionBody inside an anonymous function and execute it in the sandbox
-    const script = new vm.Script(`result = (() => { ${customFunctionBody} })();`);
-    script.runInNewContext(sandbox, { timeout: 1000 });
-
-    // Convert the result to a string representation
-    const fullResult = (sandbox.result === null || sandbox.result === undefined) ? '' : String(sandbox.result);
-    const totalPages = Math.ceil(fullResult.length / PAGE_SIZE);
-    const start = (page - 1) * PAGE_SIZE;
-    const end = page * PAGE_SIZE;
-    const stringResult = fullResult.slice(start, end);
-
-    // Generate URLs for each page
-    const pageUrls = Array.from({ length: totalPages }, (_, i) => {
-      return `${request.protocol}://${request.get('host')}${request.path}?url=${encodeURIComponent(targetUrl)}&functionBody=${encodeURIComponent(customFunctionBody)}&page=${i + 1}`;
-    });
-
-    const result = {
-      content: stringResult,
-      page: page,
-      totalPages: totalPages,
-      pageUrls: pageUrls
-    };
-
-    response.setHeader('Content-Type', 'application/json');
-    response.send(result);
-  } catch (error) {
-    if (error.message && error.message.includes('Script execution timed out')) {
-      response.status(400).send('The custom function took too long to execute.');
-    } else {
-      response.status(500).send('Error executing custom JavaScript function on website content.');
-    }
   }
 });
 
