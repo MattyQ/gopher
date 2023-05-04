b3987ae69da4fd962452c83dbc0956e4064ad122
Matty Williams <mattyqwilliams@gmail.com>
2023-05-04 12:38:12 -0700

    Update index.js
    Adding handling for different types of content, including JSON and PDFs.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const axios = require('axios');
 const { JSDOM } = require('jsdom');
 const TurndownService = require('turndown');
+const pdfParse = require('pdf-parse');
 const app = express();
 
 const rateLimitMap = new Map();
@@ -20,7 +21,9 @@ const edgeHeaders = {
   'Accept-Language': 'en-US,en;q=0.9',
   'Accept-Encoding': 'gzip, deflate, br',
   'Connection': 'keep-alive',
+  'Referer': 'https://chat.openai.com/'
 };
+
 
 app.use((req, res, next) => {
   const clientIP = req.ip;
@@ -63,27 +66,52 @@ const turndownService = new TurndownService();
 
 app.get('/fetchContent', async (request, response) => {
   const targetUrl = request.query.url;
-  const chunk = parseInt(request.query.chunk) || 1; // Renamed "page" to "chunk"
+  const chunk = parseInt(request.query.chunk) || 1;
 
   try {
-    const webContentResponse = await axios.get(targetUrl, { headers: edgeHeaders });
-    const dom = new JSDOM(webContentResponse.data);
-    const bodyContent = dom.window.document.body.outerHTML;
-    const sanitizedContent = sanitizeContent(bodyContent);
-    const markdownContent = turndownService.turndown(sanitizedContent);
-    const totalPages = Math.ceil(markdownContent.length / PAGE_SIZE);
-    const start = (chunk - 1) * PAGE_SIZE; // Updated variable name
-    const end = chunk * PAGE_SIZE; // Updated variable name
-    const contentPage = markdownContent.slice(start, end);
-    const nextPage = chunk < totalPages ? `${request.protocol}://${request.get('host')}${request.path}?url=${encodeURIComponent(targetUrl)}&chunk=${chunk + 1}` : null; // Updated parameter name
-    const morePages = totalPages - chunk; // Updated variable name
-    const nextPageMessage = morePages > 0 ? `This response has been chunked due to its length. There are ${morePages} more pages. To access the next chunk, fetch this URL: ${nextPage}\n\n` : '';
+    const webContentResponse = await axios.get(targetUrl, { headers: edgeHeaders, responseType: 'arraybuffer' });
+    const contentType = webContentResponse.headers['content-type'];
 
-    const result = nextPageMessage + "\n\n" + contentPage;
+    let result;
+
+    if (contentType.includes('text/html')) {
+      const dom = new JSDOM(webContentResponse.data);
+      const bodyContent = dom.window.document.body.outerHTML;
+      const sanitizedContent = sanitizeContent(bodyContent);
+      const markdownContent = turndownService.turndown(sanitizedContent);
+      const totalPages = Math.ceil(markdownContent.length / PAGE_SIZE);
+      const start = (chunk - 1) * PAGE_SIZE;
+      const end = chunk * PAGE_SIZE;
+      const contentPage = markdownContent.slice(start, end);
+      const nextPage = chunk < totalPages ? `${request.protocol}://${request.get('host')}${request.path}?url=${encodeURIComponent(targetUrl)}&chunk=${chunk + 1}` : null;
+      const morePages = totalPages - chunk;
+      const nextPageMessage = morePages > 0 ? `This response has been chunked due to its length. There are ${morePages} more pages. To access the next chunk, fetch this URL: ${nextPage}\n\n` : '';
+
+      result = nextPageMessage + "\n\n" + contentPage;
+
+    } else if (contentType.includes('application/json')) {
+      const jsonData = JSON.parse(webContentResponse.data.toString());
+      result = JSON.stringify(jsonData, null, 2);
+
+    } else if (contentType.startsWith('image/')) {
+      result = `![Image](${targetUrl})`;
+
+    } else if (contentType.includes('text/plain') || contentType.includes('application/xml')) {
+      result = webContentResponse.data.toString();
+
+    } else if (contentType.includes('application/pdf')) {
+      const pdfData = await pdfParse(webContentResponse.data);
+      const textContent = pdfData.text;
+      result = turndownService.turndown(textContent);
+    } else {
+      result = 'The content type of the fetched resource is not supported.';
+    }
 
     response.setHeader('Content-Type', 'text/plain');
     response.send(result);
+
   } catch (error) {
+    console.error('Error details:', error);
     response.status(500).send('Error fetching website content.');
   }
 });
